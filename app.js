1b014ed5d86c72d990d37b7c703f57cb5f4143ae
Mukho <mukho@kakao.com>
2023-07-26 22:13:57 +0900

    Version 0.3.3: 이미지 태그 복구 및 크기 제한 해제

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.use(router);
 
 server.listen(appConfig.port, () => {
   console.log(
-    `묵호 마이너 갤러리(Ver 0.3.2)\n${appConfig.port}번 포트에서 서버가 시작되었어요.`
+    `묵호 마이너 갤러리(Ver 0.3.3)\n${appConfig.port}번 포트에서 서버가 시작되었어요.`
   );
 });
