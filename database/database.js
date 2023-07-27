d32cd56c0d77291beb6df4accda86bda11329413
Mukho <mukho@kakao.com>
2023-07-27 13:43:52 +0900

    Version 0.6.0
    - MySQL 패킷 관련 문제 해결

--- database/database.js
+++ database/database.js
@@ -21,4 +21,22 @@ connection.connect((err) => {
   );
 });
 
+// Packets out of order. Got: 0 Expected: 10 오류 해결을 위한 주기적인 쿼리 전송
+function executeSelectQuery() {
+  connection.query("SELECT 1", (err) => {
+    if (err) {
+      console.error(
+        `[${getCurrentTime()}] 주기적 쿼리 전송 과정에서 오류가 발생했습니다.\n${
+          err.stack
+        }`
+      );
+      return;
+    }
+
+    console.log(`[${getCurrentTime()}] 서버는 안전합니다.`);
+  });
+}
+
+setInterval(executeSelectQuery, 30 * 60 * 1000);
+
 export default connection;
