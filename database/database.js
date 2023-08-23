62b58aa2c8266ddfb9ac11c8ffa102c8e6199e57
Mukho <mukho@kakao.com>
2023-08-24 05:52:50 +0900

    Version 1.1.3: MySQL connection lost 문제 해결

--- database/database.js
+++ database/database.js
@@ -4,39 +4,33 @@ import { getCurrentTime } from "../util/util.js";
 
 const connection = createConnection(dbConfig);
 
-connection.connect((err) => {
-  if (err) {
-    console.error(
-      `[${getCurrentTime()}] 데이터베이스 연결 과정에서 오류가 발생했습니다.\n${
-        err.stack
-      }`
-    );
-    return;
-  }
-
-  console.log(
-    `[${getCurrentTime()}] 데이터베이스가 연결되었습니다.(${
-      connection.threadId
-    })`
-  );
-});
-
-// Packets out of order. Got: 0 Expected: 10 오류 해결을 위한 주기적인 쿼리 전송
-function executeSelectQuery() {
-  connection.query("SELECT 1", (err) => {
+function handleDisconnect() {
+  connection.connect((err) => {
     if (err) {
       console.error(
-        `[${getCurrentTime()}] 주기적 쿼리 전송 과정에서 오류가 발생했습니다.\n${
+        `[${getCurrentTime()}] 데이터베이스 연결 과정에서 오류가 발생했습니다.\n${
           err.stack
         }`
       );
       return;
     }
+  
+    connection.on("error", (err) => {
+      if(err.code === "PROTOCOL_CONNECTION_LOST") {
+        return handleDisconnect();
+      } else {
+        throw err;
+      }
+    });
 
-    // console.log(`[${getCurrentTime()}] 서버는 안전합니다.`);
+    console.log(
+      `[${getCurrentTime()}] 데이터베이스가 연결되었습니다.(${
+        connection.threadId
+      })`
+    );
   });
 }
 
-setInterval(executeSelectQuery, 30 * 60 * 1000);
+handleDisconnect();
 
 export default connection;
